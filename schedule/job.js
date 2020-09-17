11bf8f6aeb590ff3d284f920c98df665da829edf
yoojh9 <yoojh9@gmail.com>
2020-09-17 10:14:59 +0900

    for heroku test

--- schedule/job.js
+++ schedule/job.js
@@ -19,7 +19,7 @@ let getBrandList = async () => {
         console.log('start getting brand list');
         let list = await request.getBrandList();
         for(let item of list) {
-            console.log('brand:'+item);
+            console.log('brand:'+item);n
             await db.setBrandList(item)
         }
     } catch(error){
