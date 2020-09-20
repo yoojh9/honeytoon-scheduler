86aea258ac0c6b691fc93df64597cca999e6b8d2
Jeonghyun <yoojh9@gmail.com>
2020-09-20 18:52:11 +0900

    랭킹 업데이트 기능 수정

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-const express = require('express');
+const express = require("express");
 const port = process.env.PORT || 3000;
-const job = require('./schedule/job');
-const schedule = require('node-schedule');
+const job = require("./schedule/job");
+const schedule = require("node-schedule");
 const server = express();
 
-var productJob = schedule.scheduleJob('5 0 * * *', job.getProductList)
-var brandJob = schedule.scheduleJob('0 0 * * *', job.getBrandList)
-var updateLeaderBoardJob = schedule.scheduleJob("15 0 * * *", job.updateLeaderBoard)
-var updateCouponSatatus = schedule.scheduleJob("30 0 * * *", job.updateCouponStatus);
+var productJob = schedule.scheduleJob("5 0 * * *", job.getProductList);
+var brandJob = schedule.scheduleJob("0 0 * * *", job.getBrandList);
+var updateLeaderBoardJob = schedule.scheduleJob(
+  "15 0 * * *",
+  job.updateLeaderBoard
+);
+var updateCouponSatatus = schedule.scheduleJob(
+  "30 0 * * *",
+  job.updateCouponStatus
+);
 
-server.listen(port, function() {
-    console.log("Example skill server listening on port " + port);
+server.listen(port, function () {
+  console.log("Example skill server listening on port " + port);
 });
 
 module.exports = server;
