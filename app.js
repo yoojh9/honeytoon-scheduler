a0ee8a1f396d55d9e4a56d59a5d1102b2e4cf83c
yoojh9 <yoojh9@gmail.com>
2020-09-25 16:07:00 +0900

    개인정보취급방침 화면 추가

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const port = process.env.PORT || 3000;
 const job = require('./schedule/job');
 const schedule = require('node-schedule');
 const server = express();
+const router = require("./routes/index");
+const path = require("path");
 
 var productJob = schedule.scheduleJob('5 0 * * *', job.getProductList)
 var brandJob = schedule.scheduleJob('0 0 * * *', job.getBrandList)
@@ -12,5 +14,10 @@ var updateCouponSatatus = schedule.scheduleJob("30 0 * * *", job.updateCouponSta
 server.listen(port, function() {
     console.log("Example skill server listening on port " + port);
 });
+
+server.set("views", path.join(__dirname, "views"));
+server.set("view engine", "ejs");
+server.use(router);
+
 
 module.exports = server;
